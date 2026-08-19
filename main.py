6874d262568e7b24347aaee5e0dcfa6411488d5b
Kacper <turbiarz80@gmail.com>
2026-08-19 19:21:46 +0200

    Fix: Moved track information to Track class

--- main.py
+++ main.py
@@ -19,12 +19,42 @@ class Tire:
     def degrade_tire(self):
         pass
 
-laps = 10
-current_lap = 1 
+
+class Track:
+    def __init__(self):
+        tracks = settings.tracks
+        self.current_track, self.total_laps = random.choice(list(tracks.items()))
+        self.is_dry = random.randint(1,10) >= 3
+        self.water_percentage = 0 if self.is_dry else random.randint(15,85)
+
+
+    def display_before_the_race_info(self):
+        if self.is_dry:
+            text_style = "bold green"
+            weather_info = "Dry"
+        else:
+            text_style = "bold blue"
+            weather_info = "Wet"
+
+        table = Table(title="Information about the race")
+        table.add_column("Track", justify="right", style="bold white")
+        table.add_column("Laps",justify="center", style="bold white")
+        table.add_column("Weather",justify="center" , style= text_style)
+        table.add_column("Water on track", justify="center", style="bold yellow")
+  
+        water_on_track = f"{self.water_percentage}%"
+
+        table.add_row(self.current_track, str(self.total_laps), weather_info, water_on_track)
+
+        print("")
+        console.print(table)
+
+
+
 grid = []
 is_race_on = 1
 
-# 1 - soft, 2 - med, 3 - hard, 4 - inter, 5 - wet
+
 
 def get_player_name():
     while True:
@@ -36,44 +66,42 @@ def get_player_name():
     return player_name
 
 
-def display_before_the_race_info():
-    r = random.randint(1,10)
-    if r >= 3:
-        is_dry = True
-        text_style = "bold green"
-    else:
-        is_dry = False
-        text_style = "bold blue"
 
-    table = Table(title="Information about the race")
-    table.add_column("Track", justify="right", style="bold white")
-    table.add_column("Laps",justify="center", style="bold white")
-    table.add_column("Weather",justify="center" , style= text_style)
-    table.add_column("Water on track", justify="center", style="bold yellow")
 
-    tracks = settings.tracks
-    track,laps = random.choice(list(tracks.items()))
-    track = str(track)
-    laps = str(laps)
 
-    if is_dry:
-        wet_percentage = 0
-        weather_info = "Dry"
-    else:
-        wet_percentage = random.randint(10,85)
-        weather_info = "Wet"
-    water_on_track = f"{wet_percentage}%"
+def get_player_tires():
+    # 1 - soft, 2 - med, 3 - hard, 4 - inter, 5 - wet
+    table = Table(title="Choose tires")
+    table.add_column("Number", justify="right")
+    table.add_column("Tire Compound")
+    table.add_column("Weather Condition")
 
-    table.add_row(track, laps, weather_info, water_on_track)
+    table.add_row("1", "Soft", "Dry", style="bold red")
+    table.add_row("2", "Medium", "Dry", style="bold yellow")
+    table.add_row("3", "Hard", "Dry", style="bold white")
+    table.add_row("4", "Inter", "Wet | 25-75%", style="bold green")
+    table.add_row("5", "Wet", "Wet | 60-100%", style="bold blue")
 
     console.print(table)
+
+    while True: 
+        tire_compound = input("Choose your tire compound (1-5): ")
+        if tire_compound in ("1", "2", "3", "4", "5"):
+            break
+        else:
+            print("Must be the number between 1 and 5.")
+        
+    return tire_compound
 
 
 def main():
     player_name = get_player_name()
 
-    display_before_the_race_info()
-   
+    track = Track()
+    track.display_before_the_race_info()
+
+    player_tires = get_player_tires()
+
     player_car = Car(driver_name = player_name, is_player_controlled = True)
     grid.append(player_car)
 
